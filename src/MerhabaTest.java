23540556734a5c8f650219a8879658c5c2d8f03a
Mehmet Almas <m.almas63@gmail.com>
2023-03-23 16:58:24 +0100

    230323-1

--- src/MerhabaTest.java
+++ src/MerhabaTest.java
@@ -5,7 +5,9 @@ public class MerhabaTest {
         String cevap = obje.selamSoyle("");
         System.out.println(cevap);
         String cevap2 = obje.selamSoyle("Esad");
+        String cevap3 = obje.selamSoyle("Ahmet");
         System.out.println(cevap2);
+        System.out.println(cevap3);
 
 
 
